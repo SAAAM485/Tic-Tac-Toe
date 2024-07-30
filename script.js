d63ebef7077ce2afc363e0343ee08a395019e52b
SAAAM485 <143632382+SAAAM485@users.noreply.github.com>
2024-07-30 12:57:43 +0800

    Completed win/draw condition.

--- script.js
+++ script.js
@@ -12,16 +12,17 @@ function Gameboard() {
 
     const getBoard = () => board;
 
-    let flag = 0;
-    const getFlag = () => flag;
+    let isAvailable = true;
+    const getIsAvailable = () => isAvailable;
 
     const dropToken = (row, column, player) => {
         if (board[row][column].getValue() === 0) {
             board[row][column].addToken(player.token);
             console.log(`Dropping ${player.name}'s token into ${row}/${column} cell...`);
+            isAvailable = true;
         } else {
             console.log(`This cell is already dropped a token! Please re-drop.`);
-            flag = 1;
+            isAvailable = false;
             return;
         }
     }
@@ -31,7 +32,7 @@ function Gameboard() {
         console.log(renderCellValues);
     }
 
-    return {getBoard, dropToken, render, getFlag};
+    return {getBoard, dropToken, render, getIsAvailable: getIsAvailable};
 }
 
 function Cell() {
@@ -72,14 +73,14 @@ function GameController(playerOneName = "P1", playerTwoName = "P2") {
         console.log(`It's ${getActivePlayer().name}'s turn.`);
     }
 
-    const rows = board.length;
-    const columns = board.length;
+    const rows = 3;
+    const columns = 3;
 
     const checkRows = (board) => {
         for (let i = 0; i < rows; i++) {
             let row = [];
             for (let j = 0; j < columns; j++) {
-                row.push(board[i][j]);    
+                row.push(board[i][j].getValue());    
             }
             if (row.every(element => element === 1) || row.every(element => element === 2)) {
                 return true;
@@ -92,7 +93,7 @@ function GameController(playerOneName = "P1", playerTwoName = "P2") {
         for (let j = 0; j < columns; j++) {
             let column = [];
             for (let i = 0; i < rows; i++) {
-                column.push(board[i][j]);    
+                column.push(board[i][j].getValue());    
             }
             if (column.every(element => element === 1) || column.every(element => element === 2)) {
                 return true;
@@ -105,12 +106,10 @@ function GameController(playerOneName = "P1", playerTwoName = "P2") {
         let diagnol1 = [];
         let diagnol2 = [];
         for (let i = 0; i < rows; i++) {
-            diagnol1.push(board[i][i]);  
+            diagnol1.push(board[i][i].getValue());  
         }
         for (let i = 0; i < rows; i++) {
-            for (let j = columns.length - 1; j < 0; j++) {
-                diagnol2.push(board[i][j]);
-            }
+            diagnol2.push(board[i][rows - i - 1].getValue());
         }
         if (diagnol1.every(element => element === 1) 
             || diagnol1.every(element => element === 2) 
@@ -130,28 +129,30 @@ function GameController(playerOneName = "P1", playerTwoName = "P2") {
     }
 
     const checkDraws = (board) => {
-        if (checkWin(board)) {
-                return false;
-        }
+        let isDraw = true;
         board.forEach(row => {
             row.forEach(cell => {
-                cell.getValue === 0;
-                return false;
+                if (cell.getValue() === 0) {
+                    isDraw = false;
+                    return isDraw;
+                }
             })
-        });
-        return true;
+        })
+        return isDraw;
     }
 
     const playRound = (row, column) => {
         board.dropToken(row, column, getActivePlayer());
-        
-        if (board.getFlag() === 0) {
-            switchPlayerTurn();
-            printNewRound();
-        } else if (board.getFlag() === 0 && checkWin(board)) {
-            console.log(`${getActivePlayer().name} wins!`);
-        } else if (board.getFlag() === 0 && checkDraws(board)) {
-            console.log(`It's a tie.`)
+
+        if (board.getIsAvailable() === true) {            
+            if (checkWin(board.getBoard())) {
+                console.log(`${getActivePlayer().name} wins!`);
+            } else if (checkDraws(board.getBoard())) {
+                console.log(`It's a tie.`)   
+            } else {
+                switchPlayerTurn();
+                printNewRound();
+            }
         }
     }
 
